d38dc9c2e6b0e9ba160b168f717a991b1d52baca
Ali Budden <alisonswick@gmail.com>
2023-02-18 10:12:31 -0600

    commit

---
+++ src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
@@ -0,0 +1,37 @@
+package com.promineotech.jeep.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import com.promineotech.jeep.controller.support.FetchJeepTestSupport;
+import com.promineotech.jeep.entity.Jeep;
+import com.promineotech.jeep.entity.JeepModel;
+
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+//random port so the tests don't run on top of each other
+class FetchJeepTest extends FetchJeepTestSupport {
+
+  @Test
+  void testThatJeepsAreReturnedWhenAValidModelandTrimAreSupplied() {
+    //Given: a valid model, trim and URI
+    JeepModel model = JeepModel.WRANGLER;
+    String trim = "Sport";
+    String uri = String.format("%s?model=%s&trim=%s", getBaseUri(), model, trim);
+    
+    System.out.println(uri);
+    
+    //When: a connection is made to the URI
+    ResponseEntity<Jeep> response = getRestTemplate().getForEntity(uri, Jeep.class);
+    
+    
+    //Then: a success (OK - 200) status code is returned.
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+  }
+
+}
