5eb86f2339a27dea6d2d84ffa5178f60336029ab
Ali Budden <alisonswick@gmail.com>
2023-02-24 13:29:46 -0600

    week 14

--- src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
+++ src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
@@ -1,22 +1,36 @@
 package com.promineotech.jeep.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-
+import java.util.List;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.jdbc.JdbcTestUtils;
 import com.promineotech.jeep.controller.support.FetchJeepTestSupport;
 import com.promineotech.jeep.entity.Jeep;
 import com.promineotech.jeep.entity.JeepModel;
 
 
+
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 //random port so the tests don't run on top of each other
+@ActiveProfiles("test")
+@Sql(scripts = {"classpath:flyway/migrations/V1.0__Jeep_Schema.sql", 
+    "classpath:flyway/migrations/V1.1__Jeep_Data.sql"},
+    config = @SqlConfig(encoding = "utf-8"))
 class FetchJeepTest extends FetchJeepTestSupport {
 
+  
   @Test
   void testThatJeepsAreReturnedWhenAValidModelandTrimAreSupplied() {
     //Given: a valid model, trim and URI
@@ -24,14 +38,21 @@ class FetchJeepTest extends FetchJeepTestSupport {
     String trim = "Sport";
     String uri = String.format("%s?model=%s&trim=%s", getBaseUri(), model, trim);
     
-    System.out.println(uri);
+//    System.out.println(uri);
     
     //When: a connection is made to the URI
-    ResponseEntity<Jeep> response = getRestTemplate().getForEntity(uri, Jeep.class);
+    ResponseEntity<List<Jeep>> response = getRestTemplate().exchange(uri, HttpMethod.GET, null,
+        new ParameterizedTypeReference<>() {});
     
     
     //Then: a success (OK - 200) status code is returned.
     assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    
+    // And: the actual list returned is the same as the expected list
+    List<Jeep> expected = buildExpected();
+    assertThat(response.getBody()).isEqualTo(expected);
   }
 
-}
+ 
+  }
+
